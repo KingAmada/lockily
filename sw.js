52110de1d08fce778c126ad8261bbda90d62912d
KingAmada <sarkiamada@gmail.com>
2026-09-11 14:08:59 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'lockily-shell-20260909-claimantdetails1';
+const CACHE_NAME = 'lockily-shell-20260911-authrestore1';
 
 self.addEventListener('install', () => {
   self.skipWaiting();
